e69e3cf42f64332acf1dcb31c3dbc256490eb9b7
Mohamed Abdallah <mohamed.abdallah@editushome.lu>
2021-12-27 00:11:57 +0100

    upd

--- ConsoleApp14/Problem/NoelTree.cs
+++ ConsoleApp14/Problem/NoelTree.cs
@@ -11,21 +11,35 @@ namespace LeetCode.Problem
         {
             Console.ForegroundColor = ConsoleColor.Green;
             Console.Write("Enter max line  : ");
-            var nbLine = int.Parse(Console.ReadLine());
+            var res = int.TryParse(Console.ReadLine(), out int nbLine);
+
+            if (!res)
+                Console.WriteLine("You must enter a valid numeric value");
+
             maxColumn = Console.LargestWindowHeight - 1;
             int centerOfScreen = maxColumn / 2;
 
-      
+
             for (int i = 0; i < nbLine; i++)
             {
                 var numberOfCharToWrite = (i * 2) + 1;
                 var position = centerOfScreen - i;
+
+                if (numberOfCharToWrite > maxColumn)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"We have cut your tree because is too BUG for my screen !, You cant exceed {maxColumn} columns");
+                    break;
+                }
+
+
+
                 WriteAtPosition(position, numberOfCharToWrite);
                 Console.WriteLine();
             }
 
             WriteRetry();
-            
+
         }
 
         private static void WriteAtPosition(int pos, int number)
